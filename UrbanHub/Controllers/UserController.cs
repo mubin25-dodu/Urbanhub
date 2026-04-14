562b1bc3f4777c0f20b5c1a6619db018f6b9a9a8
Showrav Ghosh <showravghosh1907@gmail.com>
2026-04-14 16:56:28 +0600

    Added login feature - Showrav

---
+++ UrbanHub/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+    public class UserController : Controller
+    {
+        //User/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
---
+++ UrbanHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UrbunHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
