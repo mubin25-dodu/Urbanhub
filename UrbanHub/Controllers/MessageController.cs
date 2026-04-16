3006d7e9319947294f4d02e47471f15fb203517e
Showrav Ghosh <showravghosh1907@gmail.com>
2026-04-16 19:04:01 +0600

    Added Message Design Page

---
+++ UrbanHub/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrbunHub.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("message");
+        }
+    }
+}
---
+++ UrbanHub/wwwroot/js/messages.js
@@ -0,0 +1,22 @@
+﻿const chatBody = document.getElementById('chatBody');
+const msgInput = document.getElementById('msgInput');
+
+function sendMessage() {
+    const text = msgInput.value.trim();
+    if (text === "") return;
+
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble-sent';
+    bubble.innerHTML = text;
+
+    chatBody.appendChild(bubble);
+
+    msgInput.value = "";
+    chatBody.scrollTop = chatBody.scrollHeight;
+}
+
+msgInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
