0fe7f8311233ed717358dcb5558f84b6aa29d717
Abdullah al mubin <mubin9516@gmail.com>
2026-05-09 17:46:15 +0600

    q

--- UrbanHub/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UrbunHub.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
