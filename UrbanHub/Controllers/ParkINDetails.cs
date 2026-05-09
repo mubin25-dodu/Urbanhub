1665171b7742d48b53372362273663445fadec2f
Abdullah al mubin <mubin9516@gmail.com>
2026-05-09 17:15:54 +0600

    k

---
+++ UrbanHub/Controllers/ParkINDetails.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Construction;
+using UrbanHub.DTO;
+using UrbanHub.Entities;
+using UrbanHubManagement.repo;
+
+namespace UrbanHub.web.Controllers
+{
+    public class ParkINDetails (ParkinViewDetails repo) : Controller
+    {
+        [HttpGet]
+        public IActionResult ViewDetails(int id)
+        {
+            var result = repo.GetParkingSpace(id);
+            if (result.Status)
+            {
+                return View(result.Data);
+            }
+            return NotFound();
+        }
+    }
+}
